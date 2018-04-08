ec2af9fd1ab2a73dd9e192f9433137aee80d1140
perry <User@Tipzee-Aspire.(none)>
2018-04-08 14:25:45 +0800

    month/year picker

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -19,7 +19,7 @@ export class CalendarComponent implements OnInit {
     totalDays = 35;
     days: Array<Date>;
 
-    startDateOfMonth: Date;
+    activeMonth: Date;
     daysInMonth: number;
 
     prevMonthLastDate: Date;
@@ -31,6 +31,7 @@ export class CalendarComponent implements OnInit {
     showMS = false; // Month Selection
     showYS = false; // Year Selection
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    years: Array<number>;
 
     constructor( private datepipe: DatePipe) {}
 
@@ -41,18 +42,19 @@ export class CalendarComponent implements OnInit {
     }
 
     setVars() {
+
         this.days = new Array( this.totalDays ).fill(0);
 
         if ( this.date ) {
             this.selectedDate = new Date(this.date);
             this.selectedDate.setHours(0, 0, 0, 0);
-            this.startDateOfMonth = new Date(this.date);
-            this.startDateOfMonth.setDate(1);
+            this.activeMonth = new Date(this.date);
+            this.activeMonth.setDate(1);
         }
 
         if ( !this.selectedDate ) {
-            this.startDateOfMonth = new Date( this.today );
-            this.startDateOfMonth.setDate(1);
+            this.activeMonth = new Date( this.today );
+            this.activeMonth.setDate(1);
         }
 
         if ( this.disabledDates ) {
@@ -64,44 +66,62 @@ export class CalendarComponent implements OnInit {
         }
 
         this.today.setHours(0, 0, 0, 0);
+        
+    }
+
+    createYears() {
+        const limit = 4
+        this.years = new Array(12).fill(0).map( (el, index) => {
+            if( index <= 4) {
+                el = this.activeMonth.getFullYear() - (4 - index);
+            } else {
+                el = this.activeMonth.getFullYear() + (index - 4 );
+            }
+            return el;
+        });
     }
 
     initCalendar() {
         this.setMonths();
-        this.mapDays();
+        this.setDays();
+    }
+
+    reInitCalendar() {
+        this.activeMonth = new Date(this.activeMonth);
+        this.initCalendar();
     }
 
     setMonths() {
 
         this.daysInMonth = this.getDaysInMonth(
-            this.startDateOfMonth.getMonth() + 1,
-            this.startDateOfMonth.getFullYear()
+            this.activeMonth.getMonth() + 1,
+            this.activeMonth.getFullYear()
         );
 
         this.prevMonthLastDate = new Date(
-            this.startDateOfMonth.getFullYear(),
-            this.startDateOfMonth.getMonth(), 0
+            this.activeMonth.getFullYear(),
+            this.activeMonth.getMonth(), 0
         );
 
-        this.prevMonthStartDay = this.prevMonthLastDate.getDate() - this.startDateOfMonth.getDay() + 1;
+        this.prevMonthStartDay = this.prevMonthLastDate.getDate() - this.activeMonth.getDay() + 1;
 
-        this.nextMonth = new Date( this.startDateOfMonth.getFullYear(), this.startDateOfMonth.getMonth() + 1, 1);
+        this.nextMonth = new Date( this.activeMonth.getFullYear(), this.activeMonth.getMonth() + 1, 1);
 
     }
 
-    mapDays() {
+    setDays() {
         this.nextMonthStartDay = 1;
         this.days = this.days.map( (el, index) => {
             let date;
 
-            if ( index < this.startDateOfMonth.getDay() ) {
+            if ( index < this.activeMonth.getDay() ) {
 
                 date = new Date(
                     this.prevMonthLastDate.getFullYear(),
                     this.prevMonthLastDate.getMonth(),
                     this.prevMonthStartDay + index
                 );
-            } else if ( ( index - this.startDateOfMonth.getDay() ) >= this.daysInMonth) {
+            } else if ( ( index - this.activeMonth.getDay() ) >= this.daysInMonth) {
                 date = new Date(
                     this.nextMonth.getFullYear(),
                     this.nextMonth.getMonth(),
@@ -109,9 +129,9 @@ export class CalendarComponent implements OnInit {
                 );
             } else {
                 date = new Date(
-                    this.startDateOfMonth.getFullYear(),
-                    this.startDateOfMonth.getMonth(),
-                    (index - this.startDateOfMonth.getDay() ) + 1
+                    this.activeMonth.getFullYear(),
+                    this.activeMonth.getMonth(),
+                    (index - this.activeMonth.getDay() ) + 1
                 );
             }
 
@@ -126,31 +146,34 @@ export class CalendarComponent implements OnInit {
 
     selectDay( event, date: Date ) {
         event.preventDefault();
+        event.stopPropagation();
         this.selectedDate = date;
+        
+        if( date.getMonth() > this.activeMonth.getMonth() || date.getMonth() < this.activeMonth.getMonth() ){
+            this.activeMonth.setMonth( date.getMonth() );
+            this.reInitCalendar();
+        }
         this.dateSelected.emit( date );
     }
 
-    setMonth(event, type) {
+    cycleMonth(event, type) {
         event.preventDefault();
 
         if ( type === 'next') {
-            this.startDateOfMonth.setMonth( this.startDateOfMonth.getMonth() + 1);
+            this.activeMonth.setMonth( this.activeMonth.getMonth() + 1);
         }
 
         if ( type === 'prev') {
-            this.startDateOfMonth.setMonth( this.startDateOfMonth.getMonth() - 1);
+            this.activeMonth.setMonth( this.activeMonth.getMonth() - 1);
         }
 
-        // Re-render
-        this.startDateOfMonth = new Date( this.startDateOfMonth );
-
-        this.initCalendar();
+        this.reInitCalendar();
     }
 
     getClass( date: Date) {
 
         const classes = [];
-        if ( date.getMonth() < this.startDateOfMonth.getMonth() || date.getMonth() > this.startDateOfMonth.getMonth() ) {
+        if ( date.getMonth() < this.activeMonth.getMonth() || date.getMonth() > this.activeMonth.getMonth() ) {
             classes.push('grey');
         }
 
@@ -192,10 +215,59 @@ export class CalendarComponent implements OnInit {
         event.preventDefault();
         this.hideSelections();
         this.showYS = !this.showYS;
+        if( this.showYS ) {
+            this.createYears();
+        }
     }
 
     hideSelections() {
         this.showMS = false;
         this.showYS = false;
+    }
+
+    cycleYear( type: string ) {
+        let start
+
+        if( type === 'prev') {
+            start = this.years[0] - 12;
+        }
+
+        if( type === 'next') {
+            start = this.years[this.years.length - 1] + 1;
+        }
+
+        this.years = this.years.map( (el) => {
+            return start++;
+        });
+    }
+
+    setMonth( event, month ) {
+        event.preventDefault();
+        event.stopPropagation(); // prevent click bubbling triggering top leve on-click outside.
+        const index = this.months.indexOf(month);
+        
+        if( index >= 0 && this.activeMonth.getMonth() !== index ) {
+            this.activeMonth.setMonth(index);
+            this.reInitCalendar();
+            this.hideSelections();
+        }
+    }
+
+    isSelectedMonth( month: string ) {
+        return this.activeMonth.getMonth() === this.months.indexOf(month)
+    }
+
+    isSelectedYear( year: number ) {
+        return this.activeMonth.getFullYear() === year;
+    }
+
+    setYear( event: Event, year:number ) {
+        event.preventDefault();
+        event.stopPropagation();
+        if( this.activeMonth.getFullYear() !== year ){
+            this.activeMonth.setFullYear( year );
+            this.reInitCalendar();
+            this.hideSelections();
+        }
     }
 }

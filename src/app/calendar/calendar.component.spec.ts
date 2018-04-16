9b48b4a9cc916229fa0b840cbb8123252ae54ed1
Perry <perry.mark@zeald.com>
2018-04-16 21:05:18 +0800

    set months test

--- src/app/calendar/calendar.component.spec.ts
+++ src/app/calendar/calendar.component.spec.ts
@@ -28,13 +28,30 @@ describe('CalendarComponent Tests', () => {
         expect(this.component).toBeTruthy();
     }));
 
-<<<<<<< HEAD
-    it('should initialize default variables', (async () => {
+    it('should get days in months', async(() => {
+        this.fixture.detectChanges();
+        // Get days in september 2018
+        const days = this.component.getDaysInMonth(9, 2018);
+        expect(days).toBe(30);
+    }));
 
-=======
+    // @Todo fix
+    it('should set months', async( () => {
+        const selectedDate = new Date('2018-04-23');
+        selectedDate.setHours(0, 0, 0, 0);
+
+        this.component.date = new Date(selectedDate);
+
+        this.fixture.detectChanges();
+
+        expect( this.component.activeMonth.getMonth() ).toBe( 3 );
+        expect( this.component.activeMonth.getDate() ).toBe( 1 );
+        expect( this.component.activeMonth.getFullYear() ).toBe( 2018 );
+
+    }));
+
     it('should initialize default variables', async( () => {
-        
->>>>>>> 5e680adbe8ebd29f7c99478f474494af3e3554c3
+
         const today = new Date();
         today.setHours(0, 0, 0, 0);
 
@@ -51,32 +68,6 @@ describe('CalendarComponent Tests', () => {
         expect( this.component.nextMonth.getTime() ).toBe( nextMonth.getTime() );
     }));
 
-<<<<<<< HEAD
-    it('shoud initilize variables with selected date', ( async() => {
-
-        const selectedDate = new Date('2018-08-28');
-        selectedDate.setHours(0, 0, 0, 0);
-
-        const nextMonth = new Date();
-        nextMonth.setMonth(selectedDate.getMonth() + 1);
-        nextMonth.setFullYear( selectedDate.getFullYear() );
-        nextMonth.setDate(1);
-        nextMonth.setHours(0, 0, 0, 0);
-
-        this.fixture.detectChanges();
-
-        this.component.date = selectedDate;
-
-        this.fixture.detectChanges();
-        this.component.ngOnInit();
-
-        expect(this.component.selectedDate.getTime()).toBe(selectedDate.getTime());
-        expect(this.component.nextMonth.getTime()).toBe(nextMonth.getTime());
-
-    }));
-
-});
-=======
     it('should initialize vars when date is passed', async( () => {
         const date = new Date('2018-08-28');
         date.setHours(0, 0, 0, 0);
@@ -89,7 +80,7 @@ describe('CalendarComponent Tests', () => {
         date.setDate(-2); // Jul 29 2018
         expect(this.component.days[0].date.getTime() ).toBe( date.getTime() );
     }));
-    
+
     it('should validate date object', async( () => {
         const date = new Date('2015-04-15');
         const r = this.component.isValidDateObj(date);
@@ -102,35 +93,34 @@ describe('CalendarComponent Tests', () => {
         this.component.disabledDates = ['2018-08-28', '2016-09-13', '2019-06-28'];
 
         this.fixture.detectChanges();
-        
+
         expect( this.component.disabledDates ).toEqual( disabledTimes );
     }));
 
     it('should initialize disabled dates | DateObj', async( () => {
         const disabledTimes = [1535385600000, 1473696000000, 1561651200000];
         this.component.disabledDates = [
-            new Date('2018-08-28'), 
-            new Date('2016-09-13'), 
+            new Date('2018-08-28'),
+            new Date('2016-09-13'),
             new Date('2019-06-28')
         ];
 
         this.fixture.detectChanges();
-        
+
         expect( this.component.disabledDates ).toEqual( disabledTimes );
     }));
 
     it('should initialize disabled dates | Mixed', async( () => {
-        const disabledTimes = [1535385600000, 1473696000000]; 
+        const disabledTimes = [1535385600000, 1473696000000];
 
         this.component.disabledDates = [
-            new Date('2018-08-28'), 
+            new Date('2018-08-28'),
             '2016-09-13',
             // 'nan'
         ];
 
         this.fixture.detectChanges();
-        
+
         expect( this.component.disabledDates ).toEqual( disabledTimes );
     }));
->>>>>>> 5e680adbe8ebd29f7c99478f474494af3e3554c3
-
+});
